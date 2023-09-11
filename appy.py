21c32654cb55263018b3f13fdbc2b472480fb018
Adanna Eze <10989524+adannalyn@users.noreply.github.com>
2023-09-11 07:56:30 +0100

    uppity say what

--- appy.py
+++ appy.py
@@ -1,34 +1,19 @@
 from flask import Flask, request, jsonify
-from datetime import datetime
-from flask.json import jsonify
+import datetime
+import json
+import webbrowser  # Import the webbrowser module for optional web browser opening
 
-app = Flask(__name__)
+def get_info(slack_name, track):
+    """Gets the required information and returns it in JSON format."""
+    now = datetime.datetime.utcnow()
+    utc_time = now.strftime("%Y-%m-%d %H:%M:%S")
 
-@app.route('/api', methods=['GET'])
-def get_info():
-    slack_name = request.args.get('slack_name')
-    track = request.args.get('track')
+    github_file_url = "https://github.com/your_username/repo/blob/main/backend_stage_one.py"
+    github_repo_url = "https://github.com/your_username/repo"
 
-    if slack_name is None or track is None:
-        return jsonify({"error": "Missing parameters"}), 400
-
-    if track != "backend":
-        return jsonify({"error": "Invalid track parameter"}), 400
-
-    # Get the current day of the week
-    current_day = datetime.utcnow().strftime('%A')
-
-    # Get the current UTC time with a +/-2 minute window
-    current_time_utc = datetime.utcnow()
-    utc_time = current_time_utc.strftime('%Y-%m-%d %H:%M:%S')
-
-    # GitHub URLs
-    github_file_url = "https://github.com/adannalyn/hngx/blob/main/appy.py"
-    github_repo_url = "https://github.com/adannalyn/hngx.git"
-
-    response_data = {
+    info = {
         "slack_name": slack_name,
-        "current_day": current_day,
+        "current_day": now.strftime("%A"),
         "utc_time": utc_time,
         "track": track,
         "github_file_url": github_file_url,
@@ -36,7 +21,20 @@ def get_info():
         "status_code": 200
     }
 
-    return jsonify(response_data)
+    return json.dumps(info)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+app = Flask(__name__)
+
+@app.route("/get_info", methods=["GET"])
+def get_info_endpoint():
+    slack_name = request.args.get("slack_name")
+    track = request.args.get("track")
+
+    info = get_info(slack_name, track)
+
+    return info, 200
+
+if __name__ == "__main__":
+    # Optional: Open the browser when the script is run
+    webbrowser.open("http://localhost:5000/get_info?slack_name=your_name&track=backend")
+    app.run(debug=True)
