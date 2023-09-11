49feaffcb3313abc9d86350917db12d827ea4e62
adannalyn <qvexvon@gmail.com>
2023-09-11 09:05:53 +0100

    first task

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+
+// Define a route to handle GET requests with query parameters
+app.get('/api', (req, res) => {
+  // Retrieve and validate query parameters
+  const slackName = req.query.slack_name;
+  const track = req.query.track;
+
+  if (!slackName || !track) {
+    return res.status(400).json({ error: 'Both slack_name and track are required' });
+  }
+
+  // Get the current day of the week in full
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const currentDay = daysOfWeek[new Date().getDay()];
+
+  // Get the current UTC time with +/-2 minute window
+  const currentUtcTime = new Date().toISOString();
+
+  // Define GitHub URLs (Replace with actual URLs)
+  const githubFileUrl = 'https://github.com/username/repo/blob/main/file_name.ext';
+  const githubRepoUrl = 'https://github.com/username/repo';
+
+  // Construct the JSON response
+  const responseJson = {
+    slack_name: slackName,
+    current_day: currentDay,
+    utc_time: currentUtcTime,
+    track: track,
+    github_file_url: githubFileUrl,
+    github_repo_url: githubRepoUrl,
+    status_code: 200 // Success status code
+  };
+
+  // Send the JSON response
+  res.json(responseJson);
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
