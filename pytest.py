90d13b4ca941e17007403c8ba1acc34456033bfd
Adanna Eze <10989524+adannalyn@users.noreply.github.com>
2023-09-15 22:28:54 +0100

    pytest

--- pytest.py
+++ pytest.py
@@ -1,69 +1,71 @@
 import requests
 
-# Define the API base URL
-base_url = 'http://localhost:3000/api/people'  # Replace with your API endpoint
-
-# Initialize person_id to None
-person_id = None
+# Base URL for the API
+base_url = "http://localhost:3000/api/people"
 
 # Function to create a new person
-def create_person(name):
-    data = {"name": name}
-    response = requests.post(base_url, json=data)
-    return response
+def create_person(name, age, email):
+    url = base_url
+    data = {
+        "name": name,
+        "age": age,
+        "email": email
+    }
+    response = requests.post(url, json=data)
+    print(response.status_code)
+    print(response.json())
 
 # Function to fetch details of a person
-def fetch_person(person_id):
-    url = f'{base_url}/{person_id}'
+def fetch_person(name):
+    url = f"{base_url}/{name}"
     response = requests.get(url)
-    return response
+    print(response.status_code)
+    print(response.json())
 
-# Function to modify details of an existing person
-def update_person(person_id, updated_data):
-    url = f'{base_url}/{person_id}'
-    response = requests.put(url, json=updated_data)
-    return response
+# Function to update details of an existing person
+def update_person(name, age, email):
+    url = f"{base_url}/{name}"
+    data = {
+        "age": age,
+        "email": email
+    }
+    response = requests.put(url, json=data)
+    print(response.status_code)
+    print(response.json())
 
-# Function to remove a person
-def delete_person(person_id):
-    url = f'{base_url}/{person_id}'
+# Function to delete a person
+def delete_person(name):
+    url = f"{base_url}/{name}"
     response = requests.delete(url)
-    return response
+    print(response.status_code)
 
-# Example usage
-if __name__ == "__main__":
+# Test the API
+def run_tests():
+    # Test data
+    name = "Mark Essien"
+    age = 30
+    email = "mark@example.com"
+
     # Create a new person
-    create_response = create_person("Mark Essien")
-    if create_response.status_code == 201:
-        person_id = create_response.json().get('_id')
-        if person_id:
-            print(f"Person created with ID: {person_id}")
-        else:
-            print("Response did not contain '_id' field.")
-    else:
-        print(f"Failed to create person. Status code: {create_response.status_code}")
+    create_person(name, age, email)
 
-    # Fetch details of the created person
-    if person_id:
-        fetch_response = fetch_person(person_id)
-        if fetch_response.status_code == 200:
-            person_data = fetch_response.json()
-            print("Fetch Response:", person_data)
-        else:
-            print(f"Failed to fetch person details. Status code: {fetch_response.status_code}")
+    # Fetch person details
+    fetch_person(name)
 
-        # Modify the person's details
-        if person_data:
-            update_data = {"name": "Updated Name"}
-            update_response = update_person(person_id, update_data)
-            if update_response.status_code == 200:
-                print("Update Response:", update_response.json())
-            else:
-                print(f"Failed to update person details. Status code: {update_response.status_code}")
+    # Update person details
+    new_age = 31
+    new_email = "mark.updated@example.com"
+    update_person(name, new_age, new_email)
 
-            # Remove the person
-            delete_response = delete_person(person_id)
-            if delete_response.status_code == 204:
-                print("Person removed successfully.")
-            else:
-                print(f"Failed to remove person. Status code: {delete_response.status_code}")
+    # Fetch updated person details
+    fetch_person(name)
+
+    # Delete the person
+    delete_person(name)
+
+    # Fetch person details after deletion
+    fetch_person(name)
+
+
+# Run the tests
+run_tests()
